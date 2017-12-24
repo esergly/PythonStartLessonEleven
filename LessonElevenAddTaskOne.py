f281535ec5ccdefbfa3321a5ab61fed9e4e85216
esergly <29374586+esergly@users.noreply.github.com>
2017-12-24 18:38:18 +0200

    Delete LessonElevenAddTaskOne.py

--- LessonElevenAddTaskOne.py
+++
@@ -1,9 +0,0 @@
-import itertools
-
-size = int(input('Введите размер списка: \n'))
-work = []
-for i in range(size):
-    work.append(i + 1)
-result = list(itertools.permutations(work))
-for element in result:
-    print(element)
