319ce59b6f4acae924b18688b0a19ecd6b40ad27
esergly <29374586+esergly@users.noreply.github.com>
2017-12-26 13:13:00 +0200

    Update LessonElevenAddTaskOne.py

--- LessonElevenAddTaskOne.py
+++ LessonElevenAddTaskOne.py
@@ -1,9 +1,8 @@
 import itertools
 
 size = int(input('Введите размер списка: \n'))
-work = []
-for i in range(size):
-    work.append(i + 1)
+work = list(range(1, size + 1))
 result = list(itertools.permutations(work))
 for element in result:
     print(element)
+    
